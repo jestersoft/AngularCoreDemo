cc8b4dc1be981192d1ca8436c8aa8a87f339d8ed
Tha Jester <thajesta@users.noreply.github.com>
2018-02-13 18:31:50 +0100

    Initial version

---
+++ AngularCoreDemo/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AngularCoreDemo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Redirect any non-API calls to the Angular application so our application can the routing
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                   !Path.HasExtension(context.Request.Path.Value) &&
+                   !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+
+            // Configure application for usage as API with default route of 'api/[Controller]'
+            app.UseMvcWithDefaultRoute();
+
+            // Configures application to serve the index.html file from /wwwroot when you access the server from a web browser
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
---
+++ AngularCoreDemo/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  constructor(private _httpService: Http) { }
+
+  apiValues: string[] = [];
+  menuValues: string[] = [];
+  title = "Angular with .Net Core - Demo";
+  ngOnInit() {
+    this._httpService.get('/api/values').subscribe(values => {
+      this.apiValues = values.json() as string[];
+    });
+
+    this._httpService.get('/api/menu').subscribe(values => {
+      this.menuValues = values.json() as string[];
+    });
+  }
+}
